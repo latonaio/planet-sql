907b79f00cbe1a5bb4fbfb5d9944f2a769a0dfcf
Yurino Oga <yurino.o@latona.io>
2022-01-17 16:18:40 +0900

    release

--- planet-sql-general-data.sql
+++ planet-sql-general-data.sql
@@ -1,13 +1,14 @@
 CREATE TABLE `planet_general_data`
 (
   `CodeName`                   varchar(20) NOT NULL,
+  `PlanetType`                 varchar(20) DEFAULT NULL,
   `Description`                varchar(40) DEFAULT NULL,
-  `CalenderType`               varchar(6) NOT NULL,
+  `CalenderType`               varchar(6) NDEFAULT NULL,
   `BirthYear`                  varchar(20) DEFAULT NULL,
   `EstimatedDeathYear`         varchar(80) DEFAULT NULL,
   `Status`                     varchar(20) DEFAULT NULL,
   `CreatedDateTime`            varchar(20) DEFAULT NULL,
   `LastModifiedDateTime`       varchar(20) DEFAULT NULL,
-    PRIMARY KEY (`CodeName`),
+    PRIMARY KEY (`CodeName`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
